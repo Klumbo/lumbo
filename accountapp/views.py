fa3f2696ce7830ada5aa6e805d13ab8c71f17fdb
grumpysh01 <grumpysh01@gmail.com>
2021-06-27 16:31:29 +0900

    django 21

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, HttpResponseRedirect
 from accountapp.models import HelloWorld
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 
@@ -20,3 +23,10 @@ def hello_world(request):
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp\hello_world.html', context={'hello_world_list': hello_world_list})
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:helloworld')
+    template_name = 'accountapp/create.html'
