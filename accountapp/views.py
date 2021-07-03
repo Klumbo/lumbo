3a487a3133da2566d0b31991d812f6f53a7ead1b
grumpysh01 <grumpysh01@gmail.com>
2021-07-04 03:35:23 +0900

    django 45

--- accountapp/views.py
+++ accountapp/views.py
@@ -7,7 +7,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.utils.decorators import method_decorator
 from articleapp.models import Article
-from accountapp.models import HelloWorld
 from accountapp.forms import AccountUpdateForm
 from accountapp.decorators import account_ownership_required
 # Create your views here.
@@ -15,29 +14,11 @@ from accountapp.decorators import account_ownership_required
 has_ownership = [login_required, account_ownership_required]
 
 
-@login_required
-def hello_world(request):
-    if request.method == 'POST':
-
-        temp = request.POST.get('hello_world_input')
-
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
-
-        hello_world_list = HelloWorld.objects.all()
-
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
-    else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp\hello_world.html', context={'hello_world_list': hello_world_list})
-
-
 # 아이디 비밀번호 생성
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('home')
     template_name = 'accountapp/create.html'
 
 
@@ -61,7 +42,7 @@ class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
     form_class = AccountUpdateForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('home')
     template_name = 'accountapp/update.html'
 
 
